00c8ce0f42d0cc4c64cc8e158a080463c0365d29
Arseniy <sahassser1@gmail.com>
2023-07-13 11:49:59 +0300

    Initial Commit

---
+++ MP NUMBER 1/MP_NUMBER_1App.swift
@@ -0,0 +1,17 @@
+//
+//  MP_NUMBER_1App.swift
+//  MP NUMBER 1
+//
+//  Created by 1 on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MP_NUMBER_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
